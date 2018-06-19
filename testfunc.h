df58ab9691582e821487ceae1a1d915a0d679bf8
prasanth2716 <prasanth.gunaseelan@vvdntech.in>
2018-06-19 17:42:35 +0530

    started

---
+++ testfunc.h
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testfunc.h
+ *
+ *    Description:  function definition for base prog
+ *
+ *        Version:  1.0
+ *        Created:  Tuesday 19 June 2018 05:33:58  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include<stdio.h>
+void hello()
+{
+printf("hello");
+}
---
+++ testing_1.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testing_1.c
+ *
+ *    Description:  base_prog
+ *
+ *        Version:  1.0
+ *        Created:  Tuesday 19 June 2018 05:32:17  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include<stdio.h>
+#include "testfunc.h"
+void main()
+{
+hello();
+}
